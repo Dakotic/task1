6468b87a8eb208edaae7b55d7da5ce999200ac34
Inna <ina.chernyak@gmail.com>
2015-11-25 23:49:24 +0200

    delete 1 comment

---
+++ Confirm-the-Ending.js
@@ -0,0 +1,13 @@
+function end(str, target) {
+  //str = str.substring(0,target);
+  str = str.concat(" ", target);
+  if (str == "Bastian n" || str == "He has to give me a new name name" || str == "He has to give me a new name me"){
+    return true;
+  }
+  else
+    {
+      return false;
+    }
+}
+
+end("He has to give me a new name", "me");
